7130f78d9f6d89f31395df10f63cf390152aeabc
Jinwei Chang <cwchang93@gmail.com>
2020-01-04 21:58:16 +0800

    Add Menu state

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,39 +1,52 @@
-import * as React from 'react';
-import './css.scss'
+import * as React from "react";
+import "./css.scss";
+import cx from "classnames";
 
 interface I_Nav_Props {
-    logoLink: string;
-    logoImg: string;
-    itemArr: { title: string, link: string }[];
+  logoLink: string;
+  logoImg: string;
+  itemArr: { title: string; link: string }[];
 }
 
-const Module: React.FC<I_Nav_Props> = (props) => {
+const Module: React.FC<I_Nav_Props> = props => {
+  React.useEffect(() => {
+    console.log("navBar");
+  });
 
-    React.useEffect(() => {
-        console.log('navBar')
-    })
+  const [closeMenu, setCloseMenu] = React.useState<boolean>(true);
 
-    return (
-        <div className="NavBar">
-            <a href={props.logoLink} ></a>
-
-            <ul className="itemWrap">
-                {props.itemArr.map((item: any) => {
-                    return (
-                        <li className="item">
-                            <a href={item.link}>
-                                {item.title}
-                            </a>
-                        </li>
-                    )
-                })}
-            </ul>
+  return (
+    <div className="NavBar">
+      {/* <div className="topSection"> */}
+      <div className={cx("topSection")}>
+        <div className="logo">
+          <a href={props.logoLink}></a>
         </div>
-    )
-}
 
+        <div className="clickBurgur" onClick={() => setCloseMenu(!closeMenu)}>
+          Menu
+        </div>
+      </div>
+      <ul
+        className={cx(
+          "itemWrap",
+          { displayNone: closeMenu },
+          { displayNone: closeMenu }
+        )}
+      >
+        {props.itemArr.map((item: any) => {
+          return (
+            <li className="item">
+              <a href={item.link}>{item.title}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
-export default Module
+export default Module;
 
 // import React from 'react';
 // // import './App.scss';
